dece4e56c3299352b77557b6b532136065a374fa
pguerra98 <119546662+pguerra98@users.noreply.github.com>
2023-03-29 18:28:29 -0400

    z

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const database = require(".");
 
 const connection = mysql.createConnection({
 
-    host: "localhost", port: 3306, user: "root", password: "Bebelation99!@!951753", database: "employee_tracker_db"
+    host: "localhost", port: 3306, user: "root", password: "", database: "employee_tracker_db"
 
 });
 
